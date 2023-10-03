9886f27647c0ef14abf33c2677c8c454044c8462
Nestor Petrishchev <145137300+liftoachgolf@users.noreply.github.com>
2023-10-03 15:36:09 +0300

    needfix task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <windows.h>
 #pragma warning(disable:4996)
 
 class File {
 public:
 	virtual std::string open(std::string sringline) = 0;
 	virtual double read() = 0;
-
 };
 
 class First : public File {
@@ -50,6 +51,62 @@ double First::read() {
 
 }
 
+int sign(double x) {
+	if (x > 0) {
+		return 1;
+	}
+	else if (x == 0) {
+		return 0;
+	}
+	else if (x < 0) {
+		return -1;
+	}
+}
+void USA(){
+	char one[] = "_________________________________________________\n";
+	char two[] = "********_________________________________________\n";
+	std::cout << one;
+	for (int i = 0; i < 6;++i) {
+		std::cout << two;
+	}
+	for (int i = 0; i < 6; ++i) {
+		std::cout << one;
+	}
+}
+
+void clear() {
+	for(int i = 0; i<5; ++i){
+		std::cout << std::endl << std::endl;
+	}
+}
+
+int getSin(double x, int d_y = 25) {
+	return  (int)((sin(x) * 100) + d_y / 2);
+}
+
+void gotoxy(int x, int y) {
+	COORD xy;
+	xy.X = x;
+	xy.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
+}
+
+void Sinusoid() {
+	for (double x = -100; x < 100; x += 0.01) {
+		int y = getSin(x);
+		if (y <= 1) {
+			continue;
+		}
+		gotoxy((int)(x * 10), y);
+		std::cout << '*';
+	}
+	for (int i = 0; i < 120; i++) {
+		gotoxy(i, 0);
+		std::cout << ' ';
+	}
+	gotoxy(0, 26);
+
+}
 
 
 int main() {
@@ -63,5 +120,14 @@ int main() {
 
 	double result = filee->read();
 	std::cout << result << std::endl;
+
+	std::cout << "enter x for second task: ";
+	double x;
+	std::cin >> x;
+	std::cout << "the sign of xis: " << sign(x) << std::endl;
+	USA();
+
+	Sinusoid();
+
 
 }
