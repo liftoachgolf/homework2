d200f496dba52e6bbd517250543e504d77f2fdba
Nestor Petrishchev <145137300+liftoachgolf@users.noreply.github.com>
2023-10-03 14:44:30 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -6,7 +6,7 @@
 
 class File {
 public:
-	virtual std::string open() = 0;
+	virtual std::string open(std::string sringline) = 0;
 	virtual double read() = 0;
 
 };
@@ -16,12 +16,12 @@ private:
 	std::vector<std::string>GetField;
 	std::vector<char> TField;
 public:
-	std::string open() override;
+	std::string open(std::string sringline) override;
 	double read() override;
 
 };
 
-std::string First::open()
+std::string First::open(std::string stringline)
 {
 	if (fopen("file.txt", "r") == NULL) {
 		std::ofstream oFile("file.txt");
@@ -32,7 +32,7 @@ std::string First::open()
 		out.open("file.txt");
 		if (out.is_open())
 		{
-			out << "123";	
+			out << stringline;	
 		}
 	
 	return "ss";
@@ -45,7 +45,6 @@ double First::read() {
 		while (flow >> temp) {
 			summ += temp;
 		}
-		flow.close();
 	}
 	return summ;
 
@@ -56,7 +55,12 @@ double First::read() {
 int main() {
 	First firsty;
 	File* filee = &firsty;
-	//std::string lineout = filee->open();
+	std::cout << "enter number separated by spaces:  ";
+	std::string line;
+	getline(std::cin, line);
+	std::cout << line << std::endl;
+	std::string lineout = filee->open(line);
+
 	double result = filee->read();
 	std::cout << result << std::endl;
 
